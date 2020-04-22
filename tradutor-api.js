8f28649ac2f112a4ae462923fc5d98b19f0bdf64
Daniel Gustavo Yava <Daniel.yava16@gmail.com>
2020-04-21 23:27:07 -0300

    Organizando a estutura do codigo

--- tradutor-api.js
+++ tradutor-api.js
@@ -11,24 +11,56 @@ function toCapitalizeFirstLetter(word, state = true)
 {
     var capitalizedFirstLetter;
     if(state === true){
-        capitalizeFirstLetter = word[0].toUpperCase() + word.slice(1);
+        capitalizedFirstLetter = word[0].toUpperCase() + word.slice(1);
     }else if(state === false){
-        capitalizeFirstLetter = word[0].toLowerCase() + word.slice(1);
+        capitalizedFirstLetter = word[0].toLowerCase() + word.slice(1);
 
     }
+    return capitalizedFirstLetter;
 }
+
 function toConvertTextInArrayWords(text){
     var words = text.split(' ');
     return words;
 }
-function toTranslate(from, to, text){
-    var FileDictionaryLocation =   `./languages/${from}.json`;
+
+
+function toConvertTexArrayInString(textArray = []){
+    return textArray.toString().split(',').join(' ');
+}
+
+
+function toReplaceWord(data,text, language){
+    var replacedWord = [];
+    var newText = "";
+    for(var i = 0; i < text.length; i++){
+
+        if(data[language][toCapitalizeFirstLetter(text[i],true)] === undefined){
+            replacedWord[i] = data[language][toCapitalizeFirstLetter(text[i],true)];
+        }
+        if(data[language][toCapitalizeFirstLetter(text[i],false)] !== undefined){
+            replacedWord[i] = data[language][toCapitalizeFirstLetter(text[i],false)];
+        }else{
+            replacedWord[i] = text[i];
+        }
+
+    }
+    console.log(typeof replacedWord);
+    newText = toConvertTexArrayInString(replacedWord);
+    return newText;
+}
+
+function toTranslate(fromLanguage, finalLanguage, text){
+    var FileDictionaryLocation =   `./languages/${fromLanguage}.json`;
+    text = toConvertTextInArrayWords(text);
     fetch(FileDictionaryLocation)
         .then(function(response){
             return response.json();
         })
         .then(function(data){
             console.log(data);
+            console.log(toReplaceWord(data, text, finalLanguage));
+            
         })
         .catch(function(error){
             console.error(error);
