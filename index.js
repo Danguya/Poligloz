3d682358aae8baa46c37de18666d86f9e24d8439
danguya <daniel.yava16@gmail.com>
2020-11-15 23:06:11 +0000

    Updating and Removing nodeJS

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-const express = require('express');
-const routes  = require('./routes/index');
-require('dotenv').config();
-
-const app = express();
-const PORT = process.env.PORT;
-
-app.use(routes);
-app.use(express.json());
-
-app.listen(PORT,()=>{
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+module.exports = require('./lib/poligloz');
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const index = require('../controllers/index');
-
-const routes = Router();
-
-routes.get('/', index.home);
-routes.get('/translate/:fromLanguage/:toLanguage/:content', index.translate);
-
-module.exports = routes;
